7ac9a85020462151c8a1abb824031d415f70a817
AravinthK820 <aravinth.k@unifo.in>
2023-08-09 22:56:47 +0530

    initalCommit

---
+++ packages/webapp/gatsby-node.js
@@ -0,0 +1,26 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyNode['createPages']}
+ */
+exports.onCreatePage = async ({ page, actions }) => {
+  // actions.setWebpackConfig({
+  //   node: {
+  //     fs: 'empty'
+  //   }
+  // })
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = `/app/*`
+
+    // Update the page.
+    createPage(page)
+  }
+}
